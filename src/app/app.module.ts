8e0965a7c02757e72ef62929046f872f7617971f
Paweł Kochanek <flagoon@o2.pl>
2018-03-17 22:59:59 +0100

    Fast calc layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { ScreenComponent } from './screen/screen.component';
+import { CalcButtonComponent } from './calc-button/calc-button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ScreenComponent, CalcButtonComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/calc-button/calc-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-button',
+  templateUrl: './calc-button.component.html',
+  styleUrls: ['./calc-button.component.scss']
+})
+export class CalcButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
